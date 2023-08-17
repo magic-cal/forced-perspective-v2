a726b8209810152efeff7210e5a6ee65b174e2eb
CJ Mcclure <callum.mcclure98@gmail.com>
2023-08-17 19:34:24 +0100

    Split FE and BE

---
+++ backend/index.ts
@@ -0,0 +1,39 @@
+// import express from "express";
+// // import SocketManager from "./socketManager";
+// import { createServer, Server as HttpServer } from "http";
+
+// export class ChatServer {
+//   public static readonly PORT: number = 8080;
+//   private app!: express.Application;
+//   private server!: HttpServer;
+//   private port!: string | number;
+
+//   constructor() {
+//     this.createApp();
+//     this.config();
+//     this.createServer();
+//     // this.sockets();
+//   }
+
+//   private createApp(): void {
+//     this.app = express();
+//   }
+
+//   private createServer(): void {
+//     this.server = createServer(this.app);
+//   }
+
+//   private config(): void {
+//     this.port = process.env.PORT || ChatServer.PORT;
+//   }
+
+//   //   private sockets(): void {
+//   //     new SocketManager(this.server, this.port, []);
+//   //   }
+
+//   public getApp(): express.Application {
+//     return this.app;
+//   }
+// }
+
+console.log("hi");
