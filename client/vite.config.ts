10805e9de17577cad6cb80464d84ff969781bc46
CJ Mcclure <callum.mcclure98@gmail.com>
2023-08-20 22:57:17 +0100

    Fix serving locations

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,19 @@
 /** @type {import('vite').UserConfig} */
-export default {
-  root: "src",
+
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from "vite";
+
+export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
   },
 
+  plugins: [basicSsl()],
+
   server: {
-    port: 3000,
+    // port: 3000,
+    https: true,
   },
 
   resolve: {
@@ -15,4 +21,4 @@ export default {
       "@": "/",
     },
   },
-};
+});
